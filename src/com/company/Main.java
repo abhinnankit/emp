00f63fa8f39dfa7d7db16628d74238adc2be80c3
Abhinn Ankit <ankit.a@husky.neu.edu>
2019-11-29 01:15:06 -0500

    Add gitignore file

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,7 @@
 package com.company;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
